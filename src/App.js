b5204800cfd002345bd4555bffaeb8674b568dc1
Marley Anthony <62618323+marleyanthony@users.noreply.github.com>
2022-04-17 13:07:32 -0700

    added desktop login and register button

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Sponsors from "./components/hero/Sponsors";
 import Menu from "./components/menu/Menu";
 import { useState } from "react";
 import styled from "styled-components";
+import { useEffect } from "react";
 
 const AppWrapper = styled.div`
   position: relative;
@@ -22,16 +23,30 @@ const Overlay = styled.div`
 
 function App() {
   const [menuOpen, setMenuOpen] = useState(false);
+  const [width, setWidth] = useState(0);
+
+  useEffect(() => {
+    const updateWindowDimensions = () => {
+      const newWidth = window.innerWidth;
+      setWidth(newWidth);
+    };
+
+    window.addEventListener("resize", updateWindowDimensions);
+
+    return () => window.removeEventListener("resize", updateWindowDimensions);
+  }, []);
 
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
   };
 
+  const isDesktop = width > 800 ? true : false;
+
   return (
     <AppWrapper>
       <Overlay isMenuOpen={menuOpen} />
       <Menu isMenuOpen={menuOpen} setIsMenuOpen={toggleMenu} />
-      <Navbar setIsMenuOpen={toggleMenu} />
+      <Navbar setIsMenuOpen={toggleMenu} isDesktop={isDesktop} />
       <Hero />
       <HeroSubsection />
       <Sponsors />
--- src/components/navigation/Navbar.js
+++ src/components/navigation/Navbar.js
@@ -18,13 +18,45 @@ const NavbarMenuIcon = styled.div`
   cursor: pointer;
 `;
 
-const Navbar = ({ setIsMenuOpen }) => {
+const DesktopButtonContainer = styled.div``;
+
+const LoginButton = styled.button`
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 16px;
+  border: none;
+  background: none;
+  margin: 0 10px;
+  padding: 10px 25px;
+  cursor: pointer;
+`;
+
+const RegisterButton = styled(LoginButton)`
+  border: 1px solid #000;
+  border-radius: 15px;
+  transition: 0.3s;
+
+  &:hover {
+    background-color: #000;
+    color: #fff;
+    transition: 0.3s;
+  }
+`;
+
+const Navbar = ({ setIsMenuOpen, isDesktop }) => {
   return (
     <NavbarWrapper>
       <NavbarLogo>snap</NavbarLogo>
-      <NavbarMenuIcon onClick={setIsMenuOpen}>
-        <img src={menuIcon} alt="menu icon" />
-      </NavbarMenuIcon>
+      {isDesktop ? (
+        <DesktopButtonContainer>
+          <LoginButton>Login</LoginButton>
+          <RegisterButton>Register</RegisterButton>
+        </DesktopButtonContainer>
+      ) : (
+        <NavbarMenuIcon onClick={setIsMenuOpen}>
+          <img src={menuIcon} alt="menu icon" />
+        </NavbarMenuIcon>
+      )}
     </NavbarWrapper>
   );
 };
